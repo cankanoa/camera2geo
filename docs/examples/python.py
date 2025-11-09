7144b5b22b6a5799a3e7a5fa10a3ce51aff6dd05
cankanoa <cankanoa@gmail.com>
2025-11-09 01:36:59 -1000

    Add Mavic 2 Pro example data and update Python script
    Add sample images for Mavic 2 Pro to `data_mavic2pro` directory. Update the Python example script to demonstrate camera-to-geographic transformations with new images, including lens correction and elevation data options.

--- docs/examples/python.py
+++ docs/examples/python.py
@@ -1,6 +1,32 @@
-from src.main import *
+import os
+from camera2geo import *
+
+# %% Search for cameras
+cameras = search_cameras(
+    cam_maker = "DJI",
+    cam_model = "Mavic Pro"
+)
+
+# %% Search lenses
+search_lenses(
+    cameras[0],
+    lens_maker = "DJI",
+    lens_model = "Mavic Pro"
+)
+
+# %%
+# TODO: add function to attach lense metadata to images
+
+# %% Camera space to geographic space for mavic 2 pro test images
+working_directory = os.getcwd()
 
 camera2geo(
-    input_images="/Users/kanoalindiwe/Downloads/MosaicMatch/Example1/Input/*.*",
-    output_images= "/Users/kanoalindiwe/Downloads/MosaicMatch/Example1/NewOrtho/jhgjghvb_$.tif"
+    input_images=f"{working_directory}/data_mavic2pro/input/*.*",
+    output_images= f"{working_directory}/data_mavic2pro/output/$.tif",
+    epsg = 4326,
+    correct_magnetic_declination = True,
+    cog = True,
+    image_equalize = False,
+    lens_correction = True,
+    elevation_data = True,
 )
